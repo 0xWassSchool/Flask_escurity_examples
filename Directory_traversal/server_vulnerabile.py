36b097bb7d40839d2f70018cc215242ffffc315a
0xWassSchool <123417878+0xWassSchool@users.noreply.github.com>
2023-03-06 15:39:27 +0100

    Update server_vulnerabile.py

--- Directory_traversal/server_vulnerabile.py
+++ Directory_traversal/server_vulnerabile.py
@@ -1,1 +1,15 @@
-# working in progress - Tomorrow
+import os
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    args = request.args.get('file')
+    return open(os.getcwd() + "\\" + args, 'r').read()
+
+
+if __name__ == "__main__":
+    app.run()
