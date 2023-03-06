a8747735a7528f3cb81f48062737cd801b7c90ae
0xWassSchool <123417878+0xWassSchool@users.noreply.github.com>
2023-03-06 15:39:11 +0100

    Update server_protetto.py

--- Directory_traversal/server_protetto.py
+++ Directory_traversal/server_protetto.py
@@ -1,1 +1,18 @@
-# working in progress - Tomorrow
+import os
+from flask import Flask, request, abort
+
+
+app = Flask(__name__)
+black_list = []
+
+@app.route("/")
+def index():
+    args = request.args.get('file')
+    if black_list in [*args]:
+        return abort(400)
+
+    return open(os.getcwd() + "\\" + args, 'r').read()
+
+
+if __name__ == "__main__":
+    app.run()
